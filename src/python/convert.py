b65db3b4295799b828c8a1ab5e279515e28c889e
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-08-23 18:34:54 +0530

    trying with dataframe

--- src/python/convert.py
+++ src/python/convert.py
@@ -9,14 +9,22 @@ Created on Tue Aug 23 16:53:00 2016
 ### imports
 import pandas as pd
 
+#file path
+path = "E:\\github\\ZNClub-Javascript\\MY-ASA-WORLD\\resources\\"
+file_name = path+"ASA.csv"
+print(file_name)
+
 #read
-df = pd.read_csv("E:\\github\\ZNClub-Javascript\\MY-ASA-WORLD\\resources\\ASA.csv")
+df = pd.read_csv(file_name,index_col=0)
 
 #test
-print(df.head())
+#print(df.head())
+
+##first column
+print(df[0])
 
 #write
-j = df.to_json()
+#j = df.to_json()
 
 #test
-print(j)
+#print(j)
