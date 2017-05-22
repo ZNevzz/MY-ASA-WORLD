c2086ce514792bbc83901160085179a1d848d398
ZNevzz <dsouza.nevil45@gmail.com>
2017-05-22 16:39:18 +0530

    working on appt module

--- js/main.js
+++ js/main.js
@@ -26,7 +26,35 @@ function($scope){
 		
 		}
 	}
+		
 }
 );
+
+app.controller('loginCtrl',
+function($scope){
+		
+	//$scope.username='N';
+	//$scope.password='Z';
+	
+	var usr=$scope.username;
+	var pas=$scope.password;
+	
+	$scope.auth=false;
+	console.log($scope.username+" - "+$scope.password);
+	$scope.authenticate=function(){
+		//check username and password
+		var status=auth();
+		//true then set auth to true
+		if(status==true){
+		$scope.auth=true;
+		}
+		else{
+		
+		}
+	}
+		
+}
+);
+
 
 function auth(){return true;}
