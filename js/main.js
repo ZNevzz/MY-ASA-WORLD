85400ebddc0bc43dd0b2d60ba6bc60db5ef2ba7a
ZNevzz <dsouza.nevil45@gmail.com>
2017-05-22 16:32:36 +0530

    login MVC done

--- js/main.js
+++ js/main.js
@@ -7,8 +7,26 @@ var app=angular.module('ASA',[]);
 app.controller('loginCtrl',
 function($scope){
 		
-	$scope.username='N';
-	$scope.password='Z';
+	//$scope.username='N';
+	//$scope.password='Z';
+	
+	var usr=$scope.username;
+	var pas=$scope.password;
+	
+	$scope.auth=false;
 	console.log($scope.username+" - "+$scope.password);
+	$scope.authenticate=function(){
+		//check username and password
+		var status=auth();
+		//true then set auth to true
+		if(status==true){
+		$scope.auth=true;
+		}
+		else{
+		
+		}
+	}
 }
 );
+
+function auth(){return true;}
