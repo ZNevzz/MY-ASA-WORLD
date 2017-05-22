67b1d89e82e8370ad384c049264297aad09d3f60
ZNevzz <dsouza.nevil45@gmail.com>
2017-05-22 15:11:31 +0530

    callback for ng-ctrl

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ console.log("Welcome to main.js");
 
 
 // init function
-function init(){
-	console.log("call to init.js");
-	var name="ZNevzz";
-	return name;
-}
 
+var app=angular.module('ASA',[]);
+app.controller('loginCtrl',
+function($scope){
+		
+	$scope.username='Nevil';
+	$scope.password='admin';
+	console.log($scope.username+" - "+$scope.password);
+}
+);
