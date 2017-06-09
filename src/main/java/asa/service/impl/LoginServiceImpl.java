24c14b7ace40f7952803f009e3deb699286851a7
ZNevzz <dsouza.nevil45@gmail.com>
2017-06-09 16:13:38 +0530

    tested login components:working.Added login in ng

--- src/main/java/asa/service/impl/LoginServiceImpl.java
+++ src/main/java/asa/service/impl/LoginServiceImpl.java
@@ -1,22 +1,26 @@
 package asa.service.impl;
 
 import asa.service.ConstantsInterface;
-import asa.service.LoginInterface;
+import asa.service.LoginService;
+import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
 
-public class LoginServiceImpl implements ConstantsInterface,LoginInterface{
+@Service("loginService")
+//@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+public class LoginServiceImpl implements ConstantsInterface,LoginService{
 	
 	public boolean authenticate(String username, String password){
 		
 		// TEST FOR ADMIN LOGIN
 		boolean usernameAuth=false;
 		boolean passwordAuth=false;
-		if(username.equals(ConstantsInterface.ADMIN_USR){
+		if(username.equals(ConstantsInterface.ADMIN_USR)){
 			System.out.println("username authenticated");
 			usernameAuth=true;
 			
 		}
 		
-		if(password.equals(ConstantsInterface.ADMIN_PWD){
+		if(password.equals(ConstantsInterface.ADMIN_PWD)){
 			System.out.println("password authenticated");
 			passwordAuth=true;
 		}
