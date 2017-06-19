46b0919b1d36c7015d57ec6e6c10a9a481a58b4e
ZNevzz <dsouza.nevil45@gmail.com>
2017-06-19 12:44:50 +0530

    target updated

--- target/classes/static/js/login.controller.js
+++ target/classes/static/js/login.controller.js
@@ -3,37 +3,51 @@
 app.controller('loginCtrl',
 function($scope,$http){
 		
-	var usr=$scope.username;
-	var pas=$scope.password;
-	
-
-	
-	console.log($scope.username+" - "+$scope.password);
 
 	$scope.authenticate=function(){
 		//check username and password
+		console.log($scope.username+" - "+$scope.password);
 		
-		$http.get('/login').
+		/* $http.get('/login').
         then(function(response) {
             //DO NOTHING
 			
-        });			
+        });		 */	
 		
+		
+		/* UI TEST */
+		///*
+		if($scope.username=='zn' && $scope.password=='zz')							
+		{
+			//hideAfterAuth
+			$scope.auth=true;
+			$scope.hideAfterAuth=false;
+			
+			success("authentication");
+			
+			
+		}
+		//*/
+		
+		/*
 		$http.post('/login/auth',  { 'username' : $scope.username,'password': $scope.password}).
 		then(function(response){
 			//DO 
 			success(response)
 			success(response.data['result']);
-			if(response.data['result']=='success'){
+			if(response.data['result']=='success'){				
 				$scope.auth=true;
-				
+				$scope.hideAfterAuth=false;
+				success("authentication");
 			}
 			else{
+				
+								
 				error("authentication");
 			}
 		});
-		
-		
+			
+		*/
 		
 	}
 	
