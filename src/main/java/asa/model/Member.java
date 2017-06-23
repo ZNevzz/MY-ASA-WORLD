94f129eb301832a775c0bbc47a94e56f07e4a2a5
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-06-23 16:03:24 +0530

    Created java file

---
+++ src/main/java/asa/model/Member.java
@@ -0,0 +1,95 @@
+package asa.model;
+
+
+public class Member {
+
+
+private String contact;
+
+private String role;
+
+private String gender;
+
+private String dob;
+
+private String addr;
+
+private String school;
+
+private String name;
+
+
+
+
+public String getContact() {
+return contact;
+}
+
+
+public void setContact(String contact) {
+this.contact = contact;
+}
+
+
+public String getRole() {
+return role;
+}
+
+
+public void setRole(String role) {
+this.role = role;
+}
+
+
+public String getGender() {
+return gender;
+}
+
+
+public void setGender(String gender) {
+this.gender = gender;
+}
+
+
+public String getDob() {
+return dob;
+}
+
+
+public void setDob(String dob) {
+this.dob = dob;
+}
+
+
+public String getAddr() {
+return addr;
+}
+
+
+public void setAddr(String addr) {
+this.addr = addr;
+}
+
+
+public String getSchool() {
+return school;
+}
+
+
+public void setSchool(String school) {
+this.school = school;
+}
+
+
+public String getName() {
+return name;
+}
+
+
+public void setName(String name) {
+this.name = name;
+}
+
+
+
+}
