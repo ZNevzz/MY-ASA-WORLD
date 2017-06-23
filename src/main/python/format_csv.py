90bd4acb10612d55148f9b811a3eb44d1f9a42ed
ZNevzz <dsouza.nevil45@gmail.com>
2017-06-23 15:06:40 +0530

    tested py file module, successfully

--- src/main/python/format_csv.py
+++ src/main/python/format_csv.py
@@ -13,22 +13,12 @@ dir_path='../resources/static/csv/'
 filenames=['ASA Registration Test Sample.csv']
 
 df = pd.read_csv(dir_path+filenames[0])
-#print(df.head())
-#print(df.columns)
-#print(df.Name)
 
 ##DROP UNWANTED COL AND RENAME
 
 df = df.drop(['Timestamp'],1)
-#print(df.columns)
+
 df.columns=['name','role','std','school','gender','dob','addr','contact']
-#df.set_index('name')
-
-#print(df.columns)
-#print(df.name)
-
-##TO JSON
-df.to_json('../resources/static/json/'+'ASA-2017.json')
 
 ##TO CSV
 df.to_csv(dir_path+'ASA-2017.csv',index=False)
--- src/main/python/json_collection.py
+++ src/main/python/json_collection.py
@@ -4,34 +4,8 @@ Created on Fri Jun 23 13:47:11 2017
 
 @author: ZNevzz
 """
-import json
-import pandas as pd
 
-#class Record(json.JSONEncoder):
-#    
-#    
-#    def __init__(self, name, role, std, school, gender, dob, add, contact):
-#        
-#        self.name=name
-#        self.role=role
-#        self.school=school
-#        self.gender=gender
-#        self.dob=dob
-#        self.add=add
-#        self.contact=contact
-#        
-#    def default(self,o):
-#        json.JSONEncoder.default(self, o)
-#        '''
-#        return {
-#                'name':'o.name',
-#                'role':'o.role',
-#                'school':'o.school',
-#                'gender':'o.gender',
-#                'dob':'o.dob',
-#                'add':'o.add',
-#                'contact':'o.contact',
-#                 }'''
+import pandas as pd
 
 
 ##CREATE DICT
@@ -42,43 +16,14 @@ record={'name':'','role':'','school':'gender','dob':'','addr':'','contact':''}
 
 dir_path='../resources/static/csv/'
 filenames=['ASA-2017.csv']
-
 df = pd.read_csv(dir_path+filenames[0],index_col='name') 
-#print(df.head(1))
-
-#print(df.head(1))
-
 
 ##LIST OF OBJECTS
 records=list()
 
-
-
-
-
 ##WRITE OUT
 
-#with open(dir_path[:-4]+'json/'+'ASA-2017-2.json','a+') as out:
-#    for i,r in df.iterrows():
-#        #record=Record(r.name, r.role, r.std, r.school, r.gender, r.dob, r.add, r.contact)
-#        
-#        record['name']=i
-#        record['role']=str(r.role)
-#        record['school']=r.school
-#        record['gender']=r.gender
-#        record['dob']=str(r.dob)
-#        
-#        record['addr']=str(r.addr)
-#        record['contact']=str(r.contact)        
-#        
-#        json.dump(record,out)
-#        
-#print(records[0]['addr'])
-
-
-
-for i,r in df.iterrows():
-    #record=Record(r.name, r.role, r.std, r.school, r.gender, r.dob, r.add, r.contact)
+for i,r in df.iterrows():    
     
     record['name']=i
     record['role']=str(r.role)
@@ -89,10 +34,10 @@ for i,r in df.iterrows():
     record['contact']=str(r.contact)
     records.append(record)
 
-#print(records)
+
 
 with open(dir_path[:-4]+'json/'+'ASA-2017-3.json','a+') as out:        
-    #json.dump(records,out)
+    
     out.write(str(records))
     
     
