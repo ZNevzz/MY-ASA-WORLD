8c9973338f5a48624112bfbac610d67675bb18d5
ZNevzz <dsouza.nevil45@gmail.com>
2017-06-23 20:08:21 +0530

    login hide show tested and updated

--- ui/js/login.controller.js
+++ ui/js/login.controller.js
@@ -4,6 +4,7 @@ app.controller('loginCtrl',
 function($scope,$http){
 		
 	$scope.loginObj={'status':true,'username':'','password':''};
+	$scope.error={'username':false,'password':false};
 	
 	$scope.authenticate=function(){
 		//check username and password
@@ -20,25 +21,50 @@ function($scope,$http){
 		///*
 		if($scope.loginObj.username=='zn' && $scope.loginObj.password=='zz')							
 		{
-			//hideAfterAuth
+			
 			$scope.auth=true;
 			$scope.loginObj.status=false;
-			
+			$scope.error.username=$scope.error.password=false;
 			success("authentication");
 			
 			
 		}
+		else if($scope.loginObj.password=='zz'){
+			
+			$scope.error.password=false;
+			$scope.error.username=true;
+			error("authentication in username");
+			
+		}
+		
+		else if($scope.loginObj.username=='zn'){
+			
+			$scope.error.username=false;
+			$scope.error.password=true;
+			error("authentication in password");
+			
+		}
+		else{
+			$scope.error.username=true;
+			$scope.error.password=true;
+			error("authentication in username and password");
+		}
+		
+		
 		//*/
 		
 		/*
-		$http.post('/login/auth',  { 'username' : $scope.username,'password': $scope.password}).
+		$http.post('/login/auth',  { 'username' : $scope.loginObj.username,'password': $scope.loginObj.password}).
 		then(function(response){
 			//DO 
-			success(response)
+			
 			success(response.data['result']);
-			if(response.data['result']=='success'){				
+			
+			if(response.data['result']=='success'){
+				
 				$scope.auth=true;
-				$scope.hideAfterAuth=false;
+				$scope.loginObj.status=false;
+			
 				success("authentication");
 			}
 			else{
