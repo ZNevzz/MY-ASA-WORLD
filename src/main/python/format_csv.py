a0694df0dcc7007df9263f36fcedb584f448dd3b
ZNevzz <dsouza.nevil45@gmail.com>
2017-06-23 11:36:08 +0530

    drop,rename cols

--- src/main/python/format_csv.py
+++ src/main/python/format_csv.py
@@ -7,10 +7,20 @@ Created on Fri Jun 23 11:11:22 2017
 
 import pandas as pd
 
+##READ FILE
+
 dir_path='../resources/static/csv/'
 filenames=['ASA Registration Test Sample.csv']
 
 df = pd.read_csv(dir_path+filenames[0])
+#print(df.head())
+#print(df.columns)
 
-print(df.head())
+
+##DROP UNWANTED COL AND RENAME
+
+df = df.drop(['Timestamp'],1)
+#print(df.columns)
+df.columns=['name','role','std','school','gender','dob','add','contact']
+print(df.columns)
 
