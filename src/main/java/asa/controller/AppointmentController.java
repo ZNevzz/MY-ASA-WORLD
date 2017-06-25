2184142680ac7f164193f9f7e5c3d88ef7680dc3
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-06-25 10:21:44 +0530

    Create AppointmentController.java

---
+++ src/main/java/asa/controller/AppointmentController.java
@@ -0,0 +1,33 @@
+package asa.controller;
+
+import asa.bean.Appointment;
+import asa.service.AppointmentService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+
+@RestController
+@RequestMapping("/appt")
+public class AppointmentController {
+
+	@Autowired
+	private AppointmentService appointmentService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public void view(){
+		System.out.println("Inside AppointmentController");
+	}
+	
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Map<String,String> add(@RequestBody Appointment appointment){
+        
+		  Map<String,String> map=new HashMap<>();
+		  
+      map.put("result","success");
+		  return map;
+    }
+	
+	
+
+}
