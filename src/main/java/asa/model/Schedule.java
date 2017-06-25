6062ca4a560f4747aa85640b890400cbfe19f28e
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-06-25 10:11:32 +0530

    Create Schedule.java

---
+++ src/main/java/asa/model/Schedule.java
@@ -0,0 +1,65 @@
+package asa.model;
+
+import java.util.List;
+
+public class Schedule {
+  private List<String> names;
+  private String date;
+  private String time;
+  private String sign;
+  private String type;
+  
+  public Schedule() {
+
+  }
+  
+  public List<String> getNames() {
+    return names;
+  }
+  
+
+public void setNames(List<String> names) {
+this.names = names;
+}
+
+
+public String getDate() {
+return date;
+}
+
+
+public void setDate(String date) {
+this.date = date;
+}
+
+
+public String getTime() {
+return time;
+}
+
+
+public void setTime(String time) {
+this.time = time;
+}
+
+
+public String getSign() {
+return sign;
+}
+
+
+public void setSign(String sign) {
+this.sign = sign;
+}
+
+
+public String getType() {
+return type;
+}
+
+
+public void setType(String type) {
+this.type = type;
+}
+
+}
