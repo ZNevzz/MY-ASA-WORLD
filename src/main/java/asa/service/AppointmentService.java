b457b9341d00b152bc92307d0cf8d550ca3041f9
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-06-25 10:35:27 +0530

    Update AppointmentService.java

--- src/main/java/asa/service/AppointmentService.java
+++ src/main/java/asa/service/AppointmentService.java
@@ -1,1 +1,11 @@
 
+package asa.service;
+
+import asa.bean.Appointment;
+
+public interface AppointmentService{
+	
+	public List<Appointment> get();
+	public boolean add(Appointment appointment);
+	
+}
