09d39ee09abf6974786ff6ae74c9cbc9b7fdb3c7
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-06-25 10:27:28 +0530

    Create ScheduleDAO.java

---
+++ src/main/java/asa/dao/ScheduleDAO.java
@@ -0,0 +1,16 @@
+
+package asa.dao;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import asa.model.Schedule;
+
+@Repository("scheduleDAO")
+public interface ScheduleDAO extends MongoRepository<Schedule, String> {
+
+    
+    public List<Schedule> findAll();
+
+}
